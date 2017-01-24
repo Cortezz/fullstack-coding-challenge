b7178ea316dc0d60812861d373a1e97c896599b0
Cortezz <jospcortez@gmail.com>
2017-01-24 15:55:10 +0000

    Adds threads for comment fetching

--- app/services/hacker_news.py
+++ app/services/hacker_news.py
@@ -4,12 +4,12 @@ import json
 
 hacker_news_endpoint = 'https://hacker-news.firebaseio.com/v0/'
 
-def get_all(limit):
+def get_all_posts(limit):
     response = requests.get(hacker_news_endpoint+'beststories.json').content
     data = json.loads(response)
     return data[:limit]
 
-def get(post_id):
+def get_post(post_id):
     response = requests.get(hacker_news_endpoint+'item/'+post_id+'.json').content
     data = json.loads(response)
     return {
@@ -21,3 +21,12 @@ def get(post_id):
             'author': data['by']
         }
     }
+
+def get_comment(comment_id):
+    response = requests.get(hacker_news_endpoint+'item/'+comment_id+'.json').content
+    data = json.loads(response)
+    if 'deleted' not in data:
+        return {
+            'author': data['by'],
+            'text': data['text']
+        }
--- fetch_data.py
+++ fetch_data.py
@@ -3,21 +3,37 @@ import pymongo
 from app.services import hacker_news
 from config import TOP_POSTS_LIMIT
 from pymongo import MongoClient
+import threading
+import time
 
 def save_posts():
     db = get_db()
     json = {}
     post_list = []
-    posts = hacker_news.get_all(TOP_POSTS_LIMIT)
-    for post in posts:
-        p = hacker_news.get(str(post))
-        db.stories.insert_one(p)
+    posts = hacker_news.get_all_posts(TOP_POSTS_LIMIT)
+    threads = [threading.Thread(target=insert_post, args=(db,post)) for post in posts]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
 
+def insert_post(db,post_id):
+    post = hacker_news.get_post(str(post_id))
+    print "Fetching comments for %s" % str(post_id)
+    post['comments'] = fetch_comment_data(post['post_info']['comments'])
+    db.stories.insert_one(post)
+
+def fetch_comment_data(comments_ids):
+    comments = []
+    for comment in comments_ids:
+        comments.append(hacker_news.get_comment(str(comment)))
+    return comments
 
 def get_db():
     client = MongoClient('localhost', 27017)
     db = client['multilingual-hackernews']
-    collection = db['stories']
     return db
 
+start = time.time()
 save_posts()
+print "Time elapsed: %s" % (time.time() - start)
