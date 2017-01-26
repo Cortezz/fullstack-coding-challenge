fd00bc560f597bb8f11df6bb73a87f03b26ca4a6
Cortezz <jospcortez@gmail.com>
2017-01-26 11:52:31 +0000

    Only using one posts collection

--- app/models/post.py
+++ app/models/post.py
@@ -8,4 +8,10 @@ def get(post_id):
     return db.posts.find_one({'id': post_id})
 
 def save(collection, post):
-    db[collection].insert_one(post)
+    db.posts.insert_one(post)
+
+def save_all(posts):
+    db.posts.insert_many(posts)
+
+def delete_all():
+    db.posts.delete_many({})
--- app/services/jobs.py
+++ app/services/jobs.py
@@ -7,28 +7,28 @@ import time
 
 def save_hacker_news_posts():
     print "Starting job.."
-    mongo.delete_collection('new_posts')
     posts = hacker_news_api.get_all_posts(TOP_POSTS_LIMIT)
+    new_posts = []
 
     start_time = time.time()
-    threads = [threading.Thread(target=insert_post, args=(p,)) for p in posts]
+    threads = [threading.Thread(target=insert_post, args=(p,new_posts)) for p in posts]
     for thread in threads:
         thread.start()
     for thread in threads:
         thread.join()
 
-    mongo.delete_collection('posts')
-    mongo.rename_collection('new_posts','posts')
+    post.delete_all()
+    post.save_all(new_posts)
     print("Job completed: it took %s seconds ---" % (time.time() - start_time))
 
-def insert_post(post_id):
+def insert_post(post_id, new_posts):
     p = hacker_news_api.get_post(str(post_id))
     post_title = p['title']
     uid_1 = unbabel_api.post_mt_translation(post_title, LAN_1)
     uid_2 = unbabel_api.post_mt_translation(post_title, LAN_2)
     p['comments'] = fetch_comment_data(p['comments'])
     add_translated_titles(p, {LAN_1: uid_1, LAN_2: uid_2})
-    post.save('new_posts', p)
+    new_posts.append(p)
 
 def add_translated_titles(post, uids):
     post["title_%s"%LAN_1] = unbabel_api.get_mt_translation(uids[LAN_1])
