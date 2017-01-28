212dcfaeab4d06a4a6b15b2e5fe9ccb9fc1d6079
Cortezz <jospcortez@gmail.com>
2017-01-28 01:26:50 +0000

    Removes scheduler script

--- worker.py
+++
@@ -1,7 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from app.services.jobs import save_hacker_news_posts
-
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(save_hacker_news_posts, 'interval', minutes=4)
-scheduler.start()
